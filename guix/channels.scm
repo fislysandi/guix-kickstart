6f611246ec45f78c75a6a54ca52aa02a200e1db3
fislysandi <baz.gayder@gmail.com>
2023-03-09 20:03:12 +0300

    added the public key and working on a custom upstream mirror

---
+++ guix/channels.scm
@@ -0,0 +1,10 @@
+(list (channel
+	(name 'guix)
+	(url "https://github.com/fislysandi/guix.git")
+	(branch "master")
+	(introduction
+	   (make-channel-introduction
+	     "04qkm4r50hsl3aj32amb5dczljp40zn43fjg8h3apj40bpr529w8"
+	     (openpgp-fingerprint
+	       "782B 1E59 73AE 85CE 22A2  FB29 E97B F0BC 440A 9993")))))
+	
--- scripts/guixlinks.sh
+++ scripts/guixlinks.sh
@@ -4,12 +4,12 @@
 ROOT="$(pwd | rev | cut -d '/' -f 2- | rev)"
 
 # Define the paths to the nix and nixpkgs folders
-guix_path="$ROOT/guix-config"
-
+guix_conf_path="$ROOT/guix-config"
+guix_path="$ROOT/guix"
 
 # Define the paths to the existing symlinks
-guix_link="$HOME/.config/guix-config"
-
+guix_link="$HOME/.config/guix"
+guix_conf_link="$HOME/.config/guix-config"
 
 # Check if the symlinks exist and are correct, otherwise remove them
 if [[ -L $guix_link && "$(readlink $guix_link)" == "$guix_path" ]]; then
@@ -17,8 +17,15 @@ if [[ -L $guix_link && "$(readlink $guix_link)" == "$guix_path" ]]; then
 else
     rm -rf $guix_link
     ln -s  $guix_path $guix_link
+    echo "Symlink created for guix config"
+fi
+
+if [[ -L $guix_conf_link && "$(readlink $guix_conf_link)" == "$guix_conf_path" ]]; then
+    echo "$guix_conf_link already exists and is correct"
+else
+    rm -rf $guix_conf_link
+    ln -s  $guix_conf_path $guix_conf_link
     echo "Symlink created for guix"
 fi
 
-#!/bin/bash
 
