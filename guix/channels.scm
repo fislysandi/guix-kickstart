8b7c535fece54f1b8da9a3ce34923b1bd1119e5d
fislysandi <baz.gayder@gmail.com>
2023-03-09 20:28:34 +0300

    CUSOTOM UPSTREAM REPO WORKSgit add .git add .!

--- guix/channels.scm
+++ guix/channels.scm
@@ -4,7 +4,7 @@
 	(branch "master")
 	(introduction
 	   (make-channel-introduction
-	     "04qkm4r50hsl3aj32amb5dczljp40zn43fjg8h3apj40bpr529w8"
+	     "4f4e4abd3ac3552cbf8596a50b5dade8d111571f"
 	     (openpgp-fingerprint
 	       "782B 1E59 73AE 85CE 22A2  FB29 E97B F0BC 440A 9993")))))
 	
