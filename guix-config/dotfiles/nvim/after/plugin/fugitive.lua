9201c81d0f7882d108ccffdff86560f2be88d5ef
fislysandi <baz.gayder@gmail.com>
2023-03-19 22:22:49 +0300

    more work on the neovim config

--- guix-config/dotfiles/nvim/after/plugin/fugitive.lua
+++ guix-config/dotfiles/nvim/after/plugin/fugitive.lua
@@ -1,1 +1,14 @@
 vim.keymap.set("n", "<leader>gs", vim.cmd.Git);
+
+vim.keymap.set("n", "<leader>gc", function()
+  local message = vim.fn.input("Commit message: ")
+  vim.cmd("Gwrite")
+  vim.cmd("G commit -m '" .. message .. "'")
+  vim.api.nvim_out_write("Git commit successful!\n")
+end)
+
+vim.keymap.set("n", "<leader>gp", function()
+  vim.cmd("Gpush")
+  vim.api.nvim_out_write("Git push successful!\n")
+end)
+
