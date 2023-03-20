7568f446725da54a39aecab9551ce054e7104b2a
fislysandi <baz.gayder@gmail.com>
2023-03-20 14:55:01 +0300

    Work on the Init script

--- WORKinit_guix.sh
+++ WORKinit_guix.sh
@@ -1,11 +1,19 @@
 #!/bin/bash
 
+# Create Guix directory and change ownership
+sudo mkdir -p /var/guix/profiles/per-user/$USER
+sudo chown -R $USER:$USER /var/guix/profiles/per-user/$USER
+
+# Exit immediately if any command fails
+set -e
+
+echo "Downloading Guix installer..."
 cd /tmp
-curl -L -o guix-install.sh https://git.savannah.gnu.org/cgit/guix.git/plain/etc/guix-install.sh
-chmod +x guix_install.sh
-./guix-install.sh
+sudo wget https://git.savannah.gnu.org/cgit/guix.git/plain/etc/guix-install.sh
+sudo chmod +x guix-install.sh
 
-guix pull
+echo "Running Guix installer..."
+sudo ./guix-install.sh
+
+echo "Updating package repository..."
 sudo guix pull
-
-
